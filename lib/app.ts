21e6fc93344b0d706e9bc4f5c0223a1e1e9436df
benwaples <benwaples@gmail.com>
2021-03-23 17:46:35 -0700

    add socket

--- lib/app.ts
+++ lib/app.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response, NextFunction } from 'express'
 const app = express();
+const io = require('socket.io')(app)
+
 
 app.use(express.json());
 
 // example route
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send(require('./utils/intro'))
+})
+
+io.on('connection', (socket: WebSocket) => {
+  console.log('a user has connected');
+  
 })
 
 app.use(require('./middleware/not-found'));
